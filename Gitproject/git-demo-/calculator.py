a251c02d1528794dcdc4c8b8f6ac71bb7a19355b
kavitasri <kavitasri72@gmail.com>
2025-03-23 13:36:32 +0530

    moved calculator.py

---
+++ Gitproject/git-demo-/calculator.py
@@ -0,0 +1,13 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a // b)  
+
+num = int(input("Enter a number to check odd/even: "))
+if num % 2 == 0:
+    print(f"{num} is Even")
+else:
+    print(f"{num} is Odd")
